9a91f914ea049d8decccfb0e750344014620b8e4
opdanz@gmail.com <opdanz@gmail.com>
2023-03-02 19:13:39 +0000

    --

--- models/marts/core/dim_customer.sql
+++ models/marts/core/dim_customer.sql
@@ -1,33 +1,20 @@
-
 with customers as (
-
-    select * from {{    ref('stg_customers')}}
-
+    select * from {{ ref('stg_customers')}}
 ),
-
 orders as (
-
-    select * from {{    ref('stg_orders')}}
+    select * from {{ ref('fct_orders')}}
 ),
-
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-
     group by 1
-
 ),
-
-
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -35,19 +22,8 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        sum( ford.amount) as lifetime_value
-
+        customer_orders.lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
-    join {{     ref('fact_orders')}} ford using (customer_id)
-    group by customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0)
-
 )
-
 select * from final
